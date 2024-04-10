7699d0e6a06bf2a8eef06bf7c9036728c3e66701
LillianIrivne <123099877+LillianIrivne@users.noreply.github.com>
2024-04-10 10:41:31 -0400

    Update charity_profile.js

--- charity_profile.js
+++ charity_profile.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var volunteerButton = document.getElementById("volunteer-button");
+    var volunteerForm = document.getElementById("volunteer-form");
+    volunteerButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        volunteerForm.style.display = "block";
+    });
+});
 
