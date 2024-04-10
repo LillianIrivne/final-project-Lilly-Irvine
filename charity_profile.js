459ae2bcbb1d0861abea1f22b94d9d66151932eb
LillianIrivne <123099877+LillianIrivne@users.noreply.github.com>
2024-04-10 10:51:25 -0400

    Update charity_profile.js

--- charity_profile.js
+++ charity_profile.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     var volunteerButton = document.getElementById("volunteer-button");
     var volunteerForm = document.getElementById("volunteer-form");
+    
     volunteerButton.addEventListener("click", function(event) {
         event.preventDefault();
         volunteerForm.style.display = "block";
+    });
+        
+    var charityLinks = document.querySelectorAll('.charity-link');
+    charityLinks.forEach(function(link) {
+        link.addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'green';
+        });
+        link.addEventListener('mouseout', function() {
+            this.style.backgroundColor = 'transparent';
+        });
     });
 });
 
